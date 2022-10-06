d1c830a1072122b24ba21bf2bef62c37572ddba3
shakiburcmt <shakibur.cmt@gmail.com>
2022-10-06 19:19:38 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { RouterProvider } from 'react-router-dom';
+import './App.css';
+import { routes } from './routes/routes';
+
+function App() {
+
+  return (
+    <div className="App">
+      <RouterProvider router={routes}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
